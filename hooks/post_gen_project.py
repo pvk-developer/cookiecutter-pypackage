765966a2775b052458cc336bd08c1d7ab14bf5cb
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2019-07-23 19:25:53 +0200

    Update cli.py to __main__.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,8 +15,8 @@ def main():
         remove_file('docs/authors.rst')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+        main_file = os.path.join('{{ cookiecutter.project_slug }}', '__main__.py')
+        remove_file(main_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
